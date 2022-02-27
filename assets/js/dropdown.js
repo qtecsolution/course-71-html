831d16cfa24608a4537b34db8bc6be35994a7e4f
it27472 <istiak.tushar.qtec@gmail.com>
2022-02-27 18:07:15 +0600

    star

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -23,3 +23,47 @@
 //         return false;
 //     }
 // });
+
+
+// // DEFINE CARTLINK AND WISHLINK AND CARTLIST AND WISHLIST
+// const desktopCartLink = document.querySelector('.desktop-cartlink');
+// const desktopCartList = document.querySelector('#desktop-cartlist');
+// const desktopWishLink = document.querySelector('.desktop-wishlink');
+// const desktopWishList = document.querySelector('#desktop-wishlist');
+
+// // LISTENING TO CARTLINK AND WISHLINK TRIGGERED
+// desktopCartLink.addEventListener('mouseover', showCartList);
+// desktopWishLink.addEventListener('mouseover', showWishList);
+
+// function showCartList() {
+//     desktopCartList.classList.add('show');
+// }
+
+// function showWishList() {
+//     desktopWishList.classList.add('show');
+// }
+
+// // REMOVE CARTLIST AND WISHLIST
+// const removeCartlist =  document.querySelector('#cart-remove');
+// const removeWishlist =  document.querySelector('#wish-remove');
+
+// removeCartlist.addEventListener('click', () => {
+//     desktopCartList.classList.remove('show');
+// })
+
+// removeWishlist.addEventListener('click', () => {
+//     desktopWishList.classList.remove('show');
+// })
+
+// // LISTENING TO WINDOW TRIGGERED FOR CLOSING CARTLIST AND WISHLIST
+// window.addEventListener('click', () => {
+//     desktopCartList.addEventListener('click',function(event){
+//         event.stopPropagation();
+//     });
+//     desktopCartList.classList.remove('show');
+
+//     desktopWishList.addEventListener('click',function(event){
+//         event.stopPropagation();
+//     });
+//     desktopWishList.classList.remove('show');
+// });
---
+++ assets/js/modal.js
@@ -0,0 +1,117 @@
+/**
+* // DEFINE LOGIN MODAL,
+* // REGISTER MODAL,
+* // FORGOT PASSWORD MODAL,
+* // CHANGE PASSWORD MODAL,
+* // VERIFICATIONS MODAL,
+* // CONFIRM VERIFICATIONS MODAL
+**/
+const loginModal = document.querySelector('#loginModal');
+const registerModal = document.querySelector('#register');
+const forgotPasswordModal = document.querySelector('#forgotpass');
+const changePasswordModal = document.querySelector('#changepassword');
+const verificationsModal = document.querySelector('#verifications');
+const confirmVerificationModal = document.querySelector('#verificationconfirm');
+
+/**
+* // DEFINE FORGOT BUTTON,
+* // SIGNUP BUTTON,
+* // BACK TO LOGIN BUTTON,
+* // FORGOT TO CHANGE BUTTON,
+* // CHANGE TO VERIFICATION BUTTON,
+* // CONFIRM VERIFICATIONS BUTTON
+**/
+const forgotButton = document.querySelector('#forgot-button');
+const signupButtons = Array.from(document.querySelectorAll('.btn-text'));
+const backToLoginButtons = Array.from(document.querySelectorAll('.btn-back'));
+const forgotToChangeButton = document.querySelector('#forgot-to-change');
+const changeToVerificationButton = document.querySelector('#change-to-verification');
+const verificationConfirmButton = document.querySelector('#verify-confirm');
+const signInButton = document.querySelector('#btn-signin');
+
+/**
+* // TRIGGER FORGOT BUTTON,
+* // TRIGGER SIGNUP BUTTON,
+* // TRIGGER BACK TO LOGIN BUTTON,
+* // TRIGGER FORGOT TO CHANGE BUTTON,
+* // TRIGGER CHANGE TO VERIFICATION BUTTON,
+* // TRIGGER CONFIRM VERIFICATIONS BUTTON
+**/
+forgotButton.addEventListener('click', showForgotPassword);
+forgotToChangeButton.addEventListener('click', showChangePassword);
+changeToVerificationButton.addEventListener('click', showVerification);
+verificationConfirmButton.addEventListener('click', showConfirmedChangePassword);
+signInButton.addEventListener('click', showLogin);
+
+backToLoginButtons.forEach(button => {
+    button.addEventListener('click', backToLogin);
+});
+
+signupButtons.forEach(button => {
+    button.addEventListener('click', showSignup);
+});
+
+/**
+* // LOGIN MODAL,
+* // REGISTER MODAL,
+* // FORGOT PASSWORD MODAL,
+* // CHANGE PASSWORD MODAL,
+* // VERIFICATIONS MODAL,
+* // CONFIRM VERIFICATIONS MODAL
+**/
+const login = new bootstrap.Modal(loginModal);
+const register = new bootstrap.Modal(registerModal);
+const forgot = new bootstrap.Modal(forgotPasswordModal);
+const change = new bootstrap.Modal(changePasswordModal);
+const verification = new bootstrap.Modal(verificationsModal);
+const confirmVerification = new bootstrap.Modal(confirmVerificationModal);
+
+// GO LOGIN TO FORGOT PASSWORD
+function showForgotPassword() {
+    login.hide();
+    forgot.show();
+}
+
+// SHOW SIGNUP
+function showSignup() {
+    forgot.hide();
+    change.hide();
+    verification.hide();
+    confirmVerification.hide();
+    login.hide();
+    register.show();
+}
+
+// SHOW SIGNUP
+function showLogin() {
+    login.show();
+    register.hide();
+}
+
+// BACK TO LOGIN FROM FORGOT PASSWORD
+function backToLogin() {
+    forgot.hide();
+    change.hide();
+    verification.hide();
+    confirmVerification.hide();
+    register.hide();
+    login.show();
+}
+
+// GO FORGOT PASSWORD TO CHANGE PASSWORD
+function showChangePassword() {
+    forgot.hide();
+    change.show();
+}
+
+// GO CHANGE PASSWORD TO CONFIRM
+function showConfirmedChangePassword() {
+    verification.hide();
+    confirmVerification.show();
+}
+
+// GO CHANGE PASSWORD TO VERIFICATION
+function showVerification() {
+    change.hide();
+    verification.show();
+}
