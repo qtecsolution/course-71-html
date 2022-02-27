6db31d46108532668b288c0a55abe738c7a342c1
it27472 <istiak.tushar.qtec@gmail.com>
2022-02-27 15:34:19 +0600

    home desktop banner done

---
+++ assets/js/dropdown.js
@@ -0,0 +1,25 @@
+// const dropdownLink = document.querySelector('.dropdown-link');
+// const dropdownMenu = document.querySelector('.dropdownmain');
+
+// dropdownLink.addEventListener('mouseover', () => {
+
+//     if(dropdownMenu.classList.contains('show')) {
+//         dropdownMenu.classList.remove('show');
+//     } else {
+//         dropdownMenu.classList.add('show');
+//     }
+// });
+
+// window.addEventListener('click', e => {  
+//     if(e.target !== dropdownMenu) {
+//         dropdownMenu.classList.remove('show');
+//     } else {
+//         return false;
+//     }
+
+//     if(e.target === dropdownMenu) {
+//         dropdownMenu.classList.add('show');
+//     } else {
+//         return false;
+//     }
+// });
