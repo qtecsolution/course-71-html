303da99c40176a85ddb8190b639a4a9321213a0c
it27472 <istiak.tushar.qtec@gmail.com>
2022-03-09 17:25:05 +0600

    account, certificate, mycourse completed

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,4 +1,26 @@
-const inputs = document.querySelectorAll('.inputfield');
+// ASSIGN ALL PROFILE INPUTS
+const inputs = document.querySelectorAll('.profileinput');
+// ASSIGN EDIT BUTTON
 const btnEdit = document.querySelector('#btn-edit');
+// ASSIGN CANCEL BUTTON
 const btnCancel = document.querySelector('#btn-cancel');
-// আপডেট
+
+// TRIGGER EDIT BUTTON
+btnEdit.addEventListener('click', editProfile);
+// TRIGGER CANCEL BUTTON
+btnCancel.addEventListener('click', cancelEditProfile);
+
+// EDIT PROFILE FUNCTION
+function editProfile() {
+    inputs.forEach(input => {
+        input.removeAttribute('disabled');
+    });
+    this.innerText = 'আপডেট';
+}
+
+// CANCEL PROFILE FUNCTION
+function cancelEditProfile() {
+    inputs.forEach(input => {
+        input.setAttribute('disabled', 'true');
+    });
+}
